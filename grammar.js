ce7492d2be4f8b11a3878f740809f7911ca6df69
ayecue <soerenwehmeier@googlemail.com>
2025-03-09 12:59:02 +0100

    update grammar

--- grammar.js
+++ grammar.js
@@ -2,115 +2,191 @@
 // @ts-check
 
 const PREC = {
-  OR: 1, // or
-  AND: 2, // and
-  COMPARE: 3, // < > <= >= ~= ==
-  BIT_OR: 4, // |
-  BIT_NOT: 5, // ~
-  BIT_AND: 6, // &
-  BIT_SHIFT: 7, // << >>
-  CONCAT: 8, // ..
-  PLUS: 9, // + -
-  MULTI: 10, // * / // %
-  UNARY: 11, // not # - ~
-  POWER: 12, // ^
+  OR: 1,
+  AND: 2,
+  COMPARE: 3,
+  CONCAT: 8,
+  PLUS: 9,
+  MULTI: 10,
+  UNARY: 11,
+  POWER: 12,
 };
 
-const optional_block = ($) => alias(optional($._block), $.block);
+const list_seq = (rule, separator, trailing_separator = false) =>
+  trailing_separator
+    ? seq(rule, repeat(seq(separator, rule)), optional(separator))
+    : seq(rule, repeat(seq(separator, rule)));
+
+const name_list = ($) => list_seq(choice(
+  field('name', $.identifier),
+  seq(
+    field('name', $.identifier),
+    '=',
+    field('default', $.literal)
+  )
+), ',');
 
 module.exports = grammar({
   name: 'miniscript',
 
-  extras: ($) => [$.comment, /\s/],
-
-  externals: ($) => [
-    $._block_comment_start,
-    $._block_comment_content,
-    $._block_comment_end,
-
-    $._block_string_start,
-    $._block_string_content,
-    $._block_string_end,
+  precedences: ($) => [
+    [$.arguments, $.statement_arguments],
+    [
+      $.empty_statement,
+      $.break_statement,
+      $.continue_statement,
+      $.while_statement,
+      $.if_statement_shorthand,
+      $.if_statement,
+      $.for_statement,
+      $.assignment_statement
+    ],
+    [
+      $.null,
+      $.false,
+      $.true,
+      $.number,
+      $.string,
+      $.function_definition,
+      $.variable,
+      $.function_call,
+      $.parenthesized_expression,
+      $.map_constructor,
+      $.list_constructor,
+      $.binary_expression,
+      $.unary_expression
+    ]
   ],
+
+  conflicts: ($) => [
+    [$.if_statement_shorthand],
+    [$._parameter_list, $.variable],
+    [$.return_statement],
+    [$.function_call, $.function_statement_call],
+    [$.expression, $.literal],
+    [$.function_statement_call, $.slice_expression, $.bracket_index_expression]
+  ],
+
+  extras: ($) => [$.comment, /\s/],
 
   supertypes: ($) => [$.statement, $.expression, $.variable],
 
   word: ($) => $.identifier,
 
+  externals: $ => [
+    $._command,
+    $._assignment
+  ],
+
   rules: {
     chunk: ($) =>
-      seq(
-        repeat($.statement)
-      ),
-
-    _block: ($) =>
-      choice(
-        seq(repeat1($.statement), optional($.return_statement)),
-        seq(repeat($.statement), $.return_statement)
-      ),
+      repeat($.statement),
 
     statement: ($) =>
       choice(
         $.empty_statement,
         $.break_statement,
+        $.continue_statement,
         $.while_statement,
+        $.if_statement_shorthand,
         $.if_statement,
-        $.for_statement
+        $.for_statement,
+        $.assignment_statement,
+        $.function_statement_call,
       ),
 
-    return_statement: ($) => 'return',
+    return_statement: ($) =>
+      seq(
+        'return',
+        optional($.expression),
+        optional(';')
+      ),
 
-    // ';'
     empty_statement: (_) => ';',
 
-    // break
-    break_statement: (_) => 'break',
+    assignment_statement: ($) =>
+      seq(
+        field('left', $.variable),
+        $._assignment,
+        field('right', choice($.expression, $.function_definition))
+      ),
 
-    // while exp do block end
+    break_statement: (_) => 'break',
+    continue_statement: (_) => 'continue',
+
     while_statement: ($) =>
       seq(
         'while',
         field('condition', $.expression),
-        field('body', optional_block($)),
-        'end while'
+        repeat($.statement),
+        'end',
+        'while'
       ),
 
-    // if exp then block {elseif exp then block} [else block] end
+    if_statement_shorthand: ($) =>
+      choice(
+        seq(
+          'if',
+          field('condition', $.expression),
+          'then',
+          field('consequence', choice($.expression, $.return_statement))
+        ),
+        seq(
+          'if',
+          field('condition', $.expression),
+          'then',
+          field('consequence', choice($.expression, $.return_statement)),
+          'else',
+          field('alternative', choice($.expression, $.return_statement))
+        ),
+      ),
     if_statement: ($) =>
       seq(
         'if',
         field('condition', $.expression),
         'then',
-        field('consequence', optional_block($)),
+        field('consequence', repeat($.statement)),
         repeat(field('alternative', $.elseif_statement)),
         optional(field('alternative', $.else_statement)),
-        'end if'
+        'end',
+        'if'
       ),
-    // elseif exp then block
     elseif_statement: ($) =>
       seq(
         'else if',
         field('condition', $.expression),
         'then',
-        field('consequence', optional_block($))
+        field('consequence', repeat($.statement))
       ),
-    // else block
-    else_statement: ($) => seq('else', field('body', optional_block($))),
+    else_statement: ($) => seq('else', field('body', repeat($.statement))),
 
-    // for Name '=' exp ',' exp [',' exp] do block end
-    // for namelist in explist do block end
     for_statement: ($) =>
       seq(
         'for',
         field('clause', $.for_generic_clause),
-        field('body', optional_block($)),
-        'end for'
+        field('body', repeat($.statement)),
+        'end',
+        'for'
       ),
-    // namelist in explist
     for_generic_clause: ($) =>
       seq(
-        $.variable,
-        'in'
+        $.identifier,
+        'in',
+        $.expression
+      ),
+
+    function_statement_call: ($) =>
+      choice(
+        seq(
+          field('name', $._prefix_expression),
+          field('arguments', $.statement_arguments)
+        ),
+        field('name', $._prefix_expression)
+      ),
+    statement_arguments: ($) =>
+      choice(
+        seq('(', optional(list_seq($.expression, ',')), ')'),
+        seq($._command, list_seq($.expression, ',')),
       ),
 
     expression: ($) =>
@@ -121,17 +197,19 @@ module.exports = grammar({
         $.number,
         $.string,
         $.variable,
+        $.function_call,
+        $.parenthesized_expression,
+        $.map_constructor,
+        $.list_constructor,
         $.binary_expression,
         $.unary_expression
       ),
 
-    // null
+    literal: ($) =>
+      choice($.null, $.false, $.true, $.number, $.string),
+
     null: (_) => 'null',
-
-    // false
     false: (_) => 'false',
-
-    // true
     true: (_) => 'true',
 
     // Numeral
@@ -165,17 +243,11 @@ module.exports = grammar({
         decimal_digits
       );
 
-      const hex_digits = /[a-fA-F0-9]+/;
-      const hex_literal = seq(
-        choice('0x', '0X'),
-        number_literal(hex_digits, 'p', decimal_digits)
-      );
-
-      return token(choice(decimal_literal, hex_literal));
+      return token(decimal_literal);
     },
 
     // LiteralString
-    string: ($) => choice($._quote_string, $._block_string),
+    string: ($) => $._quote_string,
 
     _quote_string: ($) =>
       choice(
@@ -186,56 +258,102 @@ module.exports = grammar({
             optional(alias($._doublequote_string_content, $.string_content))
           ),
           field('end', alias('"', '"'))
-        )
+        ),
       ),
 
     _doublequote_string_content: ($) =>
-      repeat1(choice(token.immediate(prec(1, /[^"\\]+/)), $.escape_sequence)),
-
-    _block_string: ($) =>
-      seq(
-        field('start', alias($._block_string_start, '"')),
-        field('content', alias($._block_string_content, $.string_content)),
-        field('end', alias($._block_string_end, '"'))
-      ),
+      repeat1(choice(token.immediate(prec(1, /[^"]+/)), $.escape_sequence)),
 
     escape_sequence: () =>
       token.immediate(
-        seq(
-          '\\',
-          choice(
-            /[\nabfnrtv\\'"]/,
-            /z\s*/,
-            /[0-9]{1,3}/,
-            /x[0-9a-fA-F]{2}/,
-            /u\{[0-9a-fA-F]+\}/
-          )
-        )
+        '""'
       ),
-    variable: ($) => $.identifier,
 
-    // exp binop exp
+    function_definition: ($) =>
+      seq(
+        token('function'),
+        field('parameters', optional($.parameters)),
+        field('body', repeat($.statement)),
+        token('end function')
+      ),
+    parameters: ($) => seq('(', optional($._parameter_list), ')'),
+    _parameter_list: ($) => name_list($),
+
+    _prefix_expression: ($) =>
+      prec(1, choice(
+        $.variable,
+        $.function_call,
+        $.parenthesized_expression,
+        $.literal,
+        $.map_constructor,
+        $.list_constructor,
+      )),
+
+    variable: ($) =>
+      choice(
+        $.identifier,
+        $.bracket_index_expression,
+        $.dot_index_expression,
+        $.slice_expression
+      ),
+    slice_expression: ($) =>
+      seq(
+        field('map', $._prefix_expression),
+        '[',
+        field('left', $.expression),
+        ':',
+        field('right', $.expression),
+        ']'
+      ),
+    bracket_index_expression: ($) =>
+      seq(
+        field('map', $._prefix_expression),
+        '[',
+        field('field', $.expression),
+        ']'
+      ),
+    dot_index_expression: ($) =>
+      seq(
+        field('map', $._prefix_expression),
+        '.',
+        field('field', $.identifier)
+      ),
+
+    function_call: ($) =>
+      seq(
+        field('name', $._prefix_expression),
+        field('arguments', $.arguments)
+      ),
+    arguments: ($) =>
+      seq('(', optional(list_seq($.expression, ',')), ')'),
+
+    parenthesized_expression: ($) => seq('(', $.expression, ')'),
+
+    map_constructor: ($) => seq('{', optional($._field_list), '}'),
+    _field_list: ($) => list_seq($.field, $._field_sep, true),
+    _field_sep: (_) => choice(',', ';'),
+    field: ($) =>
+      seq(field('name', $.identifier), ':', field('value', $.expression)),
+
+    list_constructor: ($) => seq('[', optional($._expression_list), ']'),
+    _expression_list: ($) => list_seq($.expression, ',', true),
+
     binary_expression: ($) =>
       choice(
         ...[
           ['or', PREC.OR],
           ['and', PREC.AND],
+          ['isa', PREC.AND],
           ['<', PREC.COMPARE],
           ['<=', PREC.COMPARE],
           ['==', PREC.COMPARE],
-          ['~=', PREC.COMPARE],
+          ['!=', PREC.COMPARE],
           ['>=', PREC.COMPARE],
           ['>', PREC.COMPARE],
-          ['|', PREC.BIT_OR],
-          ['~', PREC.BIT_NOT],
-          ['&', PREC.BIT_AND],
-          ['<<', PREC.BIT_SHIFT],
-          ['>>', PREC.BIT_SHIFT],
           ['+', PREC.PLUS],
           ['-', PREC.PLUS],
           ['*', PREC.MULTI],
           ['/', PREC.MULTI],
-          ['//', PREC.MULTI],
           ['%', PREC.MULTI],
         ].map(([operator, precedence]) =>
           prec.left(
@@ -247,28 +365,14 @@ module.exports = grammar({
             )
           )
         ),
-        ...[
-          ['^', PREC.POWER],
-        ].map(([operator, precedence]) =>
-          prec.right(
-            precedence,
-            seq(
-              field('left', $.expression),
-              operator,
-              field('right', $.expression)
-            )
-          )
-        )
       ),
 
-    // unop exp
     unary_expression: ($) =>
       prec.left(
         PREC.UNARY,
-        seq(choice('not', '@', '-', '~'), field('operand', $.expression))
+        seq(choice('not', '@', '-', '+'), field('operand', $.expression))
       ),
 
-    // Name
     identifier: (_) => {
       const identifier_start =
         /[^\p{Control}\s+\-*/%^#&~|<>=(){}\[\];:,.\\'"\d]/;
@@ -277,18 +381,10 @@ module.exports = grammar({
       return token(seq(identifier_start, identifier_continue));
     },
 
-    // comment
     comment: ($) =>
-      choice(
-        seq(
-          field('start', '//'),
-          field('content', alias(/[^\r\n]*/, $.comment_content))
-        ),
-        seq(
-          field('start', alias($._block_comment_start, '/*')),
-          field('content', alias($._block_comment_content, $.comment_content)),
-          field('end', alias($._block_comment_end, '*/'))
-        )
+      seq(
+        field('start', '//'),
+        field('content', alias(/[^\r\n]*/, $.comment_content))
       ),
   },
 });
--- src/scanner.c
+++ src/scanner.c
@@ -1,16 +1,29 @@
 #include "tree_sitter/parser.h"
 #include "tree_sitter/array.h"
+#include <wctype.h>
 
-void *tree_sitter_miniscript_external_scanner_create()
+enum TokenType
 {
-  return ts_calloc(1, sizeof(Array(int)));
+  COMMAND,
+  ASSIGNMENT
+};
+
+static inline void skip(TSLexer *lexer) { lexer->advance(lexer, true); }
+
+static inline void skip_whitespaces(TSLexer *lexer)
+{
+  while (lexer->lookahead == ' ')
+  {
+    skip(lexer);
+  }
 }
 
-void tree_sitter_miniscript_external_scanner_destroy(void *payload)
+void tree_sitter_miniscript_external_scanner_create()
 {
-  Array(int) *array = (Array(int) *)payload;
-  array_delete(array);
-  ts_free(array);
+}
+
+void tree_sitter_miniscript_external_scanner_destroy()
+{
 }
 
 bool tree_sitter_miniscript_external_scanner_scan(
@@ -18,19 +31,38 @@ bool tree_sitter_miniscript_external_scanner_scan(
     TSLexer *lexer,
     const bool *valid_symbols)
 {
-  return true;
+  if (valid_symbols[COMMAND] || valid_symbols[ASSIGNMENT])
+  {
+    bool has_leading_whitespace = lexer->lookahead == ' ';
+
+    skip_whitespaces(lexer);
+
+    if (lexer->lookahead == 0 || lexer->lookahead == NULL)
+    {
+      return false;
+    }
+
+    if (has_leading_whitespace && lexer->lookahead != '=' && valid_symbols[COMMAND])
+    {
+      lexer->result_symbol = COMMAND;
+      return true;
+    }
+    else if (lexer->lookahead == '=' && valid_symbols[ASSIGNMENT])
+    {
+      lexer->advance(lexer, false);
+      lexer->result_symbol = ASSIGNMENT;
+      return true;
+    }
+  }
+
+  return false;
 }
 
-unsigned tree_sitter_miniscript_external_scanner_serialize(
-    void *payload,
-    char *buffer)
+unsigned tree_sitter_miniscript_external_scanner_serialize()
 {
   return 0;
 }
 
-void tree_sitter_miniscript_external_scanner_deserialize(
-    void *payload,
-    const char *buffer,
-    unsigned length)
+void tree_sitter_miniscript_external_scanner_deserialize()
 {
 }
